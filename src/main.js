927a76e57c2deba38e9f22b590f0df183a181b6a
Isabel Sá <25415758+isabelsoasa@users.noreply.github.com>
2019-11-21 18:00:58 +0000

    feat(population): find objects by id and populate them with partial matches

--- src/main.js
+++ src/main.js
@@ -1,35 +1,35 @@
 import sketch from 'sketch';
-import { objFromArr } from './utils';
+import { colorsArrToObj, fontsArrToObj } from './utils';
+import { deepmerge } from './deepMerge';
 
 export default function() {
   const document = sketch.Document.getSelectedDocument();
   const colors = document.colors;
-  const symbolsMaster = document.getSymbols();
+  const symbols = document.getSymbols();
+  const textStyles = document.sharedTextStyles;
 
-  const populateTheme = () => {
+  const createTheme = () => {
     const theme = {};
 
-    const colorsUpdated = objFromArr(colors);
     const themeUpdated = {
-      colors: colorsUpdated,
-      components: nestObject(generatePathOfKeys(symbolsMaster)),
+      colors: colorsArrToObj(colors),
+      fonts: mergeAllObjects(generateFonts(textStyles)),
+      components: mergeAllObjects(generateComponents(symbols)),
     };
 
     const spreadTheme = { ...theme, ...themeUpdated };
-    return JSON.stringify(spreadTheme);
+
+    return spreadTheme;
   };
 
-  const generatePathOfKeys = symbols => {
+  const createPropertiesFromName = symbol => {
     // Takes a string such as "Button/Primary/Default" and parses it into button.primary.default.
-    const mappedSymbols = symbols.map(symbol => {
-      const nameToObjectPath = symbol.name.toLowerCase().replace(/\//g, '.');
-      return nameToObjectPath;
-    });
+    const properties = symbol.name.toLowerCase().replace(/\//g, '.');
 
-    return mappedSymbols.toString();
+    return properties.toString();
   };
 
-  const nestObject = path => {
+  const createObjectFromProperties = path => {
     // Takes a button.primary.default, parse it into an array and nests all properties.
     // Ending up with:
     // button {
@@ -43,7 +43,68 @@ export default function() {
     return nestedObject;
   };
 
-  console.log('THEME -----------------------------------------------------', populateTheme());
+  const generateComponents = () => {
+    const components = symbols.map(symbol => {
+      return createObjectFromProperties(createPropertiesFromName(symbol));
+    });
+
+    return components;
+  };
+
+  const generateFonts = textStyles => {
+    const properties = textStyles.map(font => createPropertiesFromName(font));
+    const propertiesWithoutColor = properties.map(property => property.substr(0, property.lastIndexOf('.')));
+
+    const uniqueProperties = [...new Set(propertiesWithoutColor)];
+
+    const fontStyles = uniqueProperties.map(font => createObjectFromProperties(font));
+
+    return fontStyles;
+  };
+
+  const mergeAllObjects = objects => {
+    if (objects.length > 1) {
+      return deepmerge.all(objects);
+    }
+
+    return objects;
+  };
+
+  const traverseTree = () => {
+    const theme = createTheme();
+    const keys = Object.keys(theme.fonts);
+
+    const getKeys = fontNames => {
+      return fontNames.map(key => {
+        //Get the font style names present in the theme.
+
+        const checkForKeysInTextStyles = () => {
+          // Filter by the textStyles that partially include our font style names.
+          const test = textStyles.filter(style => {
+            const textStylesNames = style.name.toLowerCase().replace(/\//g, '.');
+            return textStylesNames.includes(key);
+          });
+
+          const THEMATCH = {
+            fontSize: [...new Set(test.map(text => text.style.fontSize))].toString(),
+            textTransform: [...new Set(test.map(text => text.style.textTransform))].toString(),
+            kerning: [...new Set(test.map(text => text.style.kerning))].toString(),
+            lineHeight: [...new Set(test.map(text => text.style.lineHeight))].toString(),
+          };
+          return THEMATCH;
+        };
+
+        // MUTATE THEME
+        Object.assign(theme.fonts[key], checkForKeysInTextStyles());
+      });
+    };
+
+    getKeys(keys);
+
+    return JSON.stringify(theme);
+  };
+
+  console.log('THEME -----------------------------------------------------', traverseTree());
 
   sketch.UI.message('Copied your theme to clipboard 🎨!');
 }
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,18 @@
-export const objFromArr = (val) => {
-  const obj = val.reduce((acc, cur) => ({ ...acc, [cur.name]: cur.color }), {})
-  return obj
-}
+export const colorsArrToObj = val => {
+  const obj = val.reduce((acc, cur) => ({ ...acc, [cur.name]: cur.color }), {});
+  return obj;
+};
+
+export const fontsArrToObj = val => {
+  const obj = val.reduce(
+    (acc, cur) => ({
+      ...acc,
+      [cur.name]: {
+        fontSize: cur.style.fontSize,
+        textTransform: cur.style.textTransform,
+      },
+    }),
+    {}
+  );
+  return obj;
+};
